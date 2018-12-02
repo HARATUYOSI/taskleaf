b11f04741f14026f575d3429bf956c2463f27781
HARATUYOSI <alra9si916323@gmail.com>
2018-12-02 11:51:32 +0000

    Add user CRUD actions

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,18 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -12,6 +24,20 @@ class Admin::UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: "ユーザー[#{@user.name}]を更新しました。"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path, notice: "ユーザー[#{@user.name}]を削除しました"
+  end
   private
 
   def user_params
